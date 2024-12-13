5097ea8fba663e91cab5addbf427395461c002f2
VentCat <2546650292@qq.com>
2024-12-13 19:28:06 +0800

    fix 参数错误导致run.sh运行失败

--- run.sh
+++ run.sh
@@ -5,40 +5,40 @@ SCRIPT_PATH="/data/PRG/tools/helixfold3/apps/helixfold3_wemol/src/py_script_temp
 
 # 小型蛋白质测试
 # echo "Running small protein test..."
-# python $SCRIPT_PATH --job_name pro_300AA --protein ./test_files/protein/pro_300AA.fasta
+# $SCRIPT_PATH --job_name pro_300AA --protein ./test_files/protein/pro_300AA.fasta
 
 # 中型蛋白质测试
 # echo "Running medium protein test..."
-# python $SCRIPT_PATH --job_name pro_600AA --protein ./test_files/protein/pro_600AA.fasta
+# $SCRIPT_PATH --job_name pro_600AA --protein ./test_files/protein/pro_600AA.fasta
 
 # 蛋白质-DNA复合物测试
 # echo "Running protein-DNA complex test..."
-# python $SCRIPT_PATH --job_name pro_DNA \
+# $SCRIPT_PATH --job_name pro_DNA \
 #     --protein ./test_files/protein-DNA/pro_DNA.fasta \
 #     --dna ./test_files/protein-DNA/pro_DNA.fasta
 
 # 蛋白质-RNA复合物测试
 # echo "Running protein-RNA complex test..."
-# python $SCRIPT_PATH --job_name pro_RNA \
+# $SCRIPT_PATH --job_name pro_RNA \
 #     --protein ./test_files/protein-RNA/pro_RNA.fasta \
 #     --rna ./test_files/protein-RNA/pro_RNA.fasta
 
 # 蛋白质-配体复合物测试
 # echo "Running protein-ligand complex test..."
-# python $SCRIPT_PATH --job_name pro_lig \
+# $SCRIPT_PATH --job_name pro_lig \
 #     --protein ./test_files/protein-ligand/pro_lig.fasta \
 #     --ligand ./test_files/protein-ligand/ligand.txt
 
 # 蛋白质-配体-离子复合物测试
 echo "Running protein-ligand-ion complex test..."
-python $SCRIPT_PATH --job_name pro_lig_ion \
+$SCRIPT_PATH --job_name pro_lig_ion \
     --protein ./test_files/potein-ligand-ion/pro_lig_ion.fasta \
     --ligand ./test_files/potein-ligand-ion/ligand.txt \
     --ion "ZN"
 
 # 蛋白质-RNA-配体复合物测试
 # echo "Running protein-RNA-ligand complex test..."
-# python $SCRIPT_PATH --job_name pro_RNA_lig \
+# $SCRIPT_PATH --job_name pro_RNA_lig \
 #     --protein ./test_files/protein-RNA-ligand/pro_RNA_lig.fasta \
 #     --rna ./test_files/protein-RNA-ligand/pro_RNA_lig.fasta \
 #     --ligand ./test_files/protein-RNA-ligand/ligand.txt
