0137cc4503cdab6346c2ddd0b65ba35f56113861
VentCat <2546650292@qq.com>
2024-12-13 14:08:29 +0800

    fix pro环境下脚本运行错误问题

--- src/py_script_template/__main__.py
+++ src/py_script_template/__main__.py
@@ -1,13 +1,12 @@
+#!/data/PRG/tools/helixfold3/apps/helixfold3_wemol/.venv/bin/python
 """主入口模块
 
 用途:
     作为程序的入口点，调用主函数并处理返回值
-    
+
 使用方式:
     python -m py_script_template [参数]
 """
-
-#!.venv/Script/python
 import py_script_template
 import sys
 
